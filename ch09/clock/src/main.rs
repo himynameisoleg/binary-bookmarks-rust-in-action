2b4aa79bf66e4bac112c58004e0292a148a8f9d3
Oleg <heyitsoleg@gmail.com>
2025-03-02 10:12:48 -0600

    setting system time, UNIX and Windows

--- ch09/clock/src/main.rs
+++ ch09/clock/src/main.rs
@@ -1,5 +1,11 @@
-use chrono::DateTime;
-use chrono::Local;
+#[cfg(windows)]
+use kernel32;
+#[cfg(not(windows))]
+use libc;
+#[cfg(windows)]
+use winapi;
+
+use chrono::{DateTime, Local, TimeZone};
 use clap::{App, Arg};
 
 struct Clock;
@@ -12,6 +18,68 @@ impl Clock {
     fn set() -> ! {
         unimplemented!()
     }
+}
+
+#[cfg(not(windows))]
+fn set<Tz: TimeZone>(t: DateTime<Tz>) -> () {
+    use std::mem::zeroed;
+
+    use libc::{settimeofday, timezone};
+    use libc::{suseconds_t, time_t, timeval};
+
+    let t = t.with_timezone(&Local);
+    let mut u: timeval = unsafe { zeroed() };
+
+    u.tv_sec = t.timestamp() as time_t;
+    u.tv_usec = t.timestamp_subsec_micros() as suseconds_t;
+
+    unsafe {
+        let mock_tz: *const timezone = std::ptr::null();
+        settimeofday(&u as *const timeval, mock_tz);
+    }
+}
+
+#[cfg(windows)]
+fn set<Tz: TimeZone>(t: DateTime<Tz>) -> () {
+    use std::mem::zeroed;
+
+    use chrono::Weekday;
+    use kernel32::SetSystemTime;
+    use winapi::{SYSTEMTIME, WORD};
+
+    let t = t.with_timezone(&Local);
+
+    let mut systime: SYSTEMTIME = unsafe { zeroed() };
+
+    let dow = match t.weekday() {
+        Weekday::Mon => 1,
+        Weekday::Tue => 2,
+        Weekday::Wed => 3,
+        Weekday::Thu => 4,
+        Weekday::Fri => 5,
+        Weekday::Sat => 6,
+        Weekday::Sun => 0,
+    };
+
+    let mut ns = t.nanosecond();
+    let is_leap_second = ns > 1_000_000_000;
+
+    if is_leap_second {
+        ns -= 1_000_000_000;
+    }
+
+    systime.wYear = t.year() as WORD;
+    systime.wMonth = t.month() as WORD;
+    systime.wDayOfWeek = dow as WORD;
+    systime.wDay = t.day() as WORD;
+    systime.wHour = t.hour() as WORD;
+    systime.wMinute = t.minute() as WORD;
+    systime.wSecond = t.second() as WORD;
+    systime.wMilliseconds = (ns / 1_000_000) as WORD;
+
+    let systime_ptr = &systime as *const SYSTEMTIME;
+
+    unsafe { SetSystemTime(systime_ptr) }
 }
 
 fn main() {
