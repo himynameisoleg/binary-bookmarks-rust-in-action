4c5f37b7675acb947df1b59a570550d6e942bfde
Oleg <heyitsoleg@gmail.com>
2025-03-06 08:30:36 -0600

    error handle clock set

--- ch09/clock/src/main.rs
+++ ch09/clock/src/main.rs
@@ -15,71 +15,67 @@ impl Clock {
         Local::now()
     }
 
-    fn set() -> ! {
-        unimplemented!()
-    }
-}
+    #[cfg(not(windows))]
+    fn set<Tz: TimeZone>(t: DateTime<Tz>) -> () {
+        use std::mem::zeroed;
 
-#[cfg(not(windows))]
-fn set<Tz: TimeZone>(t: DateTime<Tz>) -> () {
-    use std::mem::zeroed;
+        use libc::{settimeofday, timezone};
+        use libc::{suseconds_t, time_t, timeval};
 
-    use libc::{settimeofday, timezone};
-    use libc::{suseconds_t, time_t, timeval};
+        let t = t.with_timezone(&Local);
+        let mut u: timeval = unsafe { zeroed() };
 
-    let t = t.with_timezone(&Local);
-    let mut u: timeval = unsafe { zeroed() };
+        u.tv_sec = t.timestamp() as time_t;
+        u.tv_usec = t.timestamp_subsec_micros() as suseconds_t;
 
-    u.tv_sec = t.timestamp() as time_t;
-    u.tv_usec = t.timestamp_subsec_micros() as suseconds_t;
-
-    unsafe {
-        let mock_tz: *const timezone = std::ptr::null();
-        settimeofday(&u as *const timeval, mock_tz);
-    }
-}
-
-#[cfg(windows)]
-fn set<Tz: TimeZone>(t: DateTime<Tz>) -> () {
-    use std::mem::zeroed;
-
-    use chrono::Weekday;
-    use kernel32::SetSystemTime;
-    use winapi::{SYSTEMTIME, WORD};
-
-    let t = t.with_timezone(&Local);
-
-    let mut systime: SYSTEMTIME = unsafe { zeroed() };
-
-    let dow = match t.weekday() {
-        Weekday::Mon => 1,
-        Weekday::Tue => 2,
-        Weekday::Wed => 3,
-        Weekday::Thu => 4,
-        Weekday::Fri => 5,
-        Weekday::Sat => 6,
-        Weekday::Sun => 0,
-    };
-
-    let mut ns = t.nanosecond();
-    let is_leap_second = ns > 1_000_000_000;
-
-    if is_leap_second {
-        ns -= 1_000_000_000;
+        unsafe {
+            let mock_tz: *const timezone = std::ptr::null();
+            settimeofday(&u as *const timeval, mock_tz);
+        }
     }
 
-    systime.wYear = t.year() as WORD;
-    systime.wMonth = t.month() as WORD;
-    systime.wDayOfWeek = dow as WORD;
-    systime.wDay = t.day() as WORD;
-    systime.wHour = t.hour() as WORD;
-    systime.wMinute = t.minute() as WORD;
-    systime.wSecond = t.second() as WORD;
-    systime.wMilliseconds = (ns / 1_000_000) as WORD;
+    #[cfg(windows)]
+    fn set<Tz: TimeZone>(t: DateTime<Tz>) -> () {
+        use std::mem::zeroed;
 
-    let systime_ptr = &systime as *const SYSTEMTIME;
+        use chrono::Weekday;
+        use kernel32::SetSystemTime;
+        use winapi::{SYSTEMTIME, WORD};
 
-    unsafe { SetSystemTime(systime_ptr) }
+        let t = t.with_timezone(&Local);
+
+        let mut systime: SYSTEMTIME = unsafe { zeroed() };
+
+        let dow = match t.weekday() {
+            Weekday::Mon => 1,
+            Weekday::Tue => 2,
+            Weekday::Wed => 3,
+            Weekday::Thu => 4,
+            Weekday::Fri => 5,
+            Weekday::Sat => 6,
+            Weekday::Sun => 0,
+        };
+
+        let mut ns = t.nanosecond();
+        let is_leap_second = ns > 1_000_000_000;
+
+        if is_leap_second {
+            ns -= 1_000_000_000;
+        }
+
+        systime.wYear = t.year() as WORD;
+        systime.wMonth = t.month() as WORD;
+        systime.wDayOfWeek = dow as WORD;
+        systime.wDay = t.day() as WORD;
+        systime.wHour = t.hour() as WORD;
+        systime.wMinute = t.minute() as WORD;
+        systime.wSecond = t.second() as WORD;
+        systime.wMilliseconds = (ns / 1_000_000) as WORD;
+
+        let systime_ptr = &systime as *const SYSTEMTIME;
+
+        unsafe { SetSystemTime(systime_ptr) }
+    }
 }
 
 fn main() {
@@ -111,7 +107,18 @@ fn main() {
     let std = args.value_of("std").unwrap();
 
     if action == "set" {
-        unimplemented!()
+        let t_ = args.value_of("datetime").unwrap();
+
+        let parser = match std {
+            "rfc2822" => DateTime::parse_from_rfc2822,
+            "rfc3339" => DateTime::parse_from_rfc3339,
+            _ => unimplemented!(),
+        };
+
+        let err_msg = format!("Unable to parse {} according to {}", t_, std);
+        let t = parser(t_).expect(&err_msg);
+
+        Clock::set(t);
     }
 
     let now = Clock::get();
