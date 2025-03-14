4ed40edbdfbb2b151037a7121048bc6c15a56251
Oleg <heyitsoleg@gmail.com>
2025-03-14 16:47:27 -0500

    spawning multiple threads

--- ch10/threads/src/main.rs
+++ ch10/threads/src/main.rs
@@ -3,12 +3,18 @@ use std::{thread, time};
 fn main() {
     let start = time::Instant::now();
 
-    let handler = thread::spawn(|| {
+    let handler_1 = thread::spawn(|| {
         let pause = time::Duration::from_millis(300);
         thread::sleep(pause.clone());
     });
 
-    handler.join().unwrap();
+    let handler_2 = thread::spawn(|| {
+        let pause = time::Duration::from_millis(300);
+        thread::sleep(pause.clone());
+    });
+
+    handler_1.join().unwrap();
+    handler_2.join().unwrap();
 
     let finish = time::Instant::now();
 
