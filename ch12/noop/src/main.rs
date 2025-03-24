92c9ed911dd1b3957ba6b6ac1161c598a68c19db
Oleg <heyitsoleg@gmail.com>
2025-03-24 17:21:24 -0500

    add typed ptr

--- ch12/noop/src/main.rs
+++ ch12/noop/src/main.rs
@@ -2,6 +2,8 @@ fn noop() {}
 
 fn main() {
     let fn_ptr = noop as usize;
+    let typed_fn_ptr = noop as *const fn() -> ();
 
     println!("noop in usise: 0x{:x}", fn_ptr);
+    println!("noop as * const T: {:p}", typed_fn_ptr);
 }
